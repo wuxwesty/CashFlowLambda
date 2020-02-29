a80af6ba081bcf1830e3c49f9bcc72eb7fe5fb16
root <rlwest@yahoo.com>
2020-02-29 17:29:10 -0500

    Add build warnings flag

--- ci/concourse/build.sh
+++ ci/concourse/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-repo/gradlew --build-file repo/build.gradle build --warning-mode all
+repo/gradlew --build-file repo/build.gradle build --warning-mode all --stacktrace
 cp repo/Services/build/libs/*.jar out/
